00efd1edfd1afea2ab628598efbb29e9201a2273
tristanlatr <tris.la.tr@gmail.com>
2019-07-15 23:40:48 -0400

    Generate docs

---
+++ update.sh
@@ -0,0 +1,11 @@
+mkdir temp && cd temp
+git clone https://github.com/mfesiem/msiempy && cd msiempy
+pdoc msiempy --output-dir ../../docs --html
+pyreverse -s 1 -f PUB_ONLY -o png -m y msiempy
+mv ./classes.png ../../docs/msiempy
+mv ./packages.png ../../docs/msiempy
+cd ../.. && rm -rf temp
+git add .
+git commit -m 'Generate docs'
+git push origin master
+git status
